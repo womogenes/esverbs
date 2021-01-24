dcd1e7cd9cd4928219306255a38c60ac072129cf
William Y. Feng <williamfeng1729@gmail.com>
2021-01-24 15:58:40 -0800

    Make it stable!

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+// Load the library
+let conjugator = new jsESverb();
+
+const update = verb => {
+  let result = conjugator.conjugate(verb);
+    
+  for (let form in result) {
+    $(`#${form}`).text(result[form]);
+  }
+
+  $('#verb-input').select();
+  localStorage.setItem('verb', $('#verb-input').val());
+}
+
+$('#verb-input').on('keyup', e => {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    update($('#verb-input').val());
+  }
+});
+
 // Focus on text box when loaded
 $('#verb-input').focus();
+
+// Get verb from local storage
+let storedVerb = localStorage.getItem('verb');
+if (storedVerb) {
+  $('#verb-input').val(storedVerb);
+  $('#verb-input').select();
+  update(storedVerb);
+}
